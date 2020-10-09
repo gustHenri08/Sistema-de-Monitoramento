1a5c691a6fe5c0296769a27181aee373bcf78518
mateusmax076 <72174578+mateusmax076@users.noreply.github.com>
2020-10-08 21:29:20 -0300

    primeira commit

---
+++ sql/mateus.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 10/09/2017 às 15:54
+-- Versão do servidor: 5.7.14
+-- Versão do PHP: 7.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `mateus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `horarios`
+--
+
+CREATE TABLE `horarios` (
+  `id` int(11) NOT NULL,
+    `cadeia` varchar(220) NOT NULL,
+  `nomepri` varchar(220) NOT NULL,
+  `nomem` varchar(220) NOT NULL,
+  `nomep` varchar(220) NOT NULL,
+  `motivo` varchar(220) NOT NULL,
+  `tipo` varchar(220) NOT NULL,
+  `obs_gerais` varchar(220) NOT NULL,
+     `datai` datetime NOT NULL,
+    `datas` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Fazendo dump de dados para tabela `horarios`
+--
+
+INSERT INTO `horarios` (`id`, `datai`, `datas`) VALUES
+(1, '2017-01-10 09:45:44', '2017-09-10 09:45:44'),
+(2, '2017-01-23 09:00:32', '2017-09-23 09:00:32');
+
+--
+-- Índices de tabelas apagadas
+--
+
+--
+-- Índices de tabela `horarios`
+--
+ALTER TABLE `horarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas apagadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `horarios`
+--
+ALTER TABLE `horarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
